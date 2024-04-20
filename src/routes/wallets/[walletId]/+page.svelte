9ede0787b09cb12543a3449dd3a5fe326d8a3495
duck_master <duckeagle1280@gmail.com>
2024-04-20 12:18:21 -0400

    use native svelte routing

---
+++ src/routes/wallets/[walletId]/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import {page} from '$app/stores';
+    import { onMount } from 'svelte';
+
+    let wallet: Record<string, string>
+
+    $: walletId = $page.params.walletId;
+
+    // TODO
+    async function getWallet(id: string) {
+        return {
+            "id": id,
+            "lorem": "ipsum"
+        }
+    }
+
+    onMount(async() => {
+        wallet = await getWallet(walletId);
+    })
+</script>
+
+{JSON.stringify(wallet)}
+
+
+
+
+
+
