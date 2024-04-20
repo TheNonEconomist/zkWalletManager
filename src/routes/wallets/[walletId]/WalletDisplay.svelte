65c7a332fd5d4c7a1e862f54b798f3de214d88fb
duck_master <duckeagle1280@gmail.com>
2024-04-20 15:49:43 -0400

    add section headers to WalletDisplay.svelte

--- src/routes/wallets/[walletId]/WalletDisplay.svelte
+++ src/routes/wallets/[walletId]/WalletDisplay.svelte
@@ -2,8 +2,20 @@
     export let wallet;
 </script>
 
-<!-- header -->
+<!-- header-->
 <h1>{wallet["id"]}</h1>
 
-<!-- other links-->
+<!-- blockchain links-->
+<h2>Other wallets</h2>
+<ul>
+    <li>{JSON.stringify(wallet["chains"])}</li>
+</ul>
+
+<!-- external links-->
+<h2>External links</h2>
+<ul>
+    <li>{JSON.stringify(wallet["extlinks"])}</li>
+</ul>
+
+<!-- sanity check-->
 <p>{JSON.stringify(wallet)}</p>
