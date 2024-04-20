f76728277d897de4486c125f85a11a45df096ce5
duck_master <duckeagle1280@gmail.com>
2024-04-20 17:37:07 -0400

    make a better about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,7 +6,15 @@
 <Navbar />
 
 <div class = "main">
-    <h1>About Page</h1>
+    <h1>About</h1>
 
-    <p>Lorem ipsum</p>
+    <p>This website was built for the <a href = "https://mitbitcoin.devpost.com/">2024 MIT Bitcoin Hackathon.</a> on April 19 - 20, 2024.</p>
+
+    <p>Contact:</p>
+
+    <ul>
+        <li>duck_master (<a href = "mailto:duckmaster0@protonmail.com">duckmaster0@protonmail.com</a>)</li>
+        <li>The Non-Economist (<a href = "mailto:thenoneconomist@gmail.com">thenoneconomist@gmail.com</a>)</li>
+        <li>Ilya Gulko (<a href = "mailto:breakfast@gmail.com">breakfast@gmail.com</a>)</li>
+    </ul>
 </div>
