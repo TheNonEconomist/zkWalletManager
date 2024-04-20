d49338b4bbb44382e77aadecd4d1411ac6bb9243
duck_master <duckeagle1280@gmail.com>
2024-04-19 23:25:57 -0400

    modify landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class = "header">
+    <h1>zkwalletmanager</h1>
+</div>
+
+<div class = "main">
+    <h2>Your hub for blockchain users</h2>
+    <p>Link your wallet to other accounts you have. Look up other people's wallets. Selectively reveal info about yourself to your friends or the world.</p>
+</div>
