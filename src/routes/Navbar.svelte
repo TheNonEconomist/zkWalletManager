e62771246dc116a3b3da529831543fc3ccc72bec
duck_master <duckeagle1280@gmail.com>
2024-04-20 16:49:41 -0400

    add navbar to /about; add <nav> to navbar

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,3 +1,7 @@
 <div class = "header">
     <h1>zkwalletmanager</h1>
+    <nav>
+        <a href = "/">Home</a>
+        <a href = "/about">About</a>
+    </nav>
 </div>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,12 @@
-<h1>About Page</h1>
+<script>
+    import Navbar from "../Navbar.svelte";
+</script>
 
-<p>Lorem ipsum</p>
+
+<Navbar />
+
+<div class = "main">
+    <h1>About Page</h1>
+
+    <p>Lorem ipsum</p>
+</div>
